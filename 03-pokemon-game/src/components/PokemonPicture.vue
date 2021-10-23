7ab98bb179ef8ab91651644eb84aa428a965e893
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2021-10-23 17:30:38 -0500

    Design components

--- 03-pokemon-game/src/components/PokemonPicture.vue
+++ 03-pokemon-game/src/components/PokemonPicture.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-      <h3>Pokemon Picture</h3>
+    <div class="pokemon-container">
+      <img
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+        class="hidden-pokemon"
+        alt="pokemon">
+      <img
+         src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+         class="fade-in" 
+         alt="pokemon">
+    </div>
+
   </div>
 </template>
 
@@ -10,6 +20,22 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.pokemon-container {
+    height: 200px;
+}
+img {
+    height: 200px;
+    position: absolute;
+    width: 100vw;
+    margin-left: -50vw;
+    user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    -webkit-user-drag: none;
+    -webkit-user-select: none;
+}
+.hidden-pokemon {
+    filter: brightness(0);
+}
 </style>
