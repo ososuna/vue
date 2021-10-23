a93894067de93bcb59a08e50d8d3255e1c642644
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2021-10-23 01:25:34 -0500

    Component data

--- 01-basics/app.js
+++ 01-basics/app.js
@@ -3,6 +3,15 @@ const app = Vue.createApp({
         <h1>Hello World</h1>
         <p>From app.js</p>
     ` */
+
+    data() {
+        return {
+            message: 'Hello World',
+            quote: 'I am Batman',
+            author: 'Bruce Wayne'
+        }
+    }
+
 });
 
 app.mount('#myApp');
