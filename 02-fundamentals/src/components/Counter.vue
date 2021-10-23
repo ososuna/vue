1b01f95eb815c25a0ef39626ad5bdb3645ef33a3
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2021-10-23 03:28:41 -0500

    Added methods to counter

--- 02-fundamentals/src/components/Counter.vue
+++ 02-fundamentals/src/components/Counter.vue
@@ -1,16 +1,55 @@
 <template>
     <div>
         <h2>Counter</h2>
-        <p>10</p>
+        <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
+        <div>
+            <button @click="increase()">+1</button>
+            <button @click="decrease()">-1</button>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'Counter'
+    name: 'Counter',
+    data() {
+        return {
+            counter: 5
+        }
+    },
+    methods: {
+        getSquareValue() {
+            return this.counter * this.counter;
+        },
+        increase() {
+            this.counter ++;
+        },
+        decrease() {
+            this.counter --;
+        }
+    },
+    computed: {
+        squareCounter() {
+            return this.counter * this.counter;
+        }
+    }
 }
 </script>
 
 <style>
+button{
+    background-color: #64BB87;
+    border-radius: 5px;
+    border: 1px solid white;
+    color: white;
+    cursor: pointer;
+    margin: 0 5px;
+    padding: 5px 15px;
+    transition: 0.3s ease-in-out;
+}
 
+button:hover {
+    background-color: #5aa67b;
+    transition: 0.3 ease-in-out;
+}
 </style>
