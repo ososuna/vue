6ef600f1e52feb2d4a45b089391304f369170b1d
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2021-10-26 21:00:37 -0500

    Create get pokemon names logic

--- 03-pokemon-game/src/helpers/getPokemonOptions.js
+++ 03-pokemon-game/src/helpers/getPokemonOptions.js
@@ -3,8 +3,22 @@ const getPokemons = () => {
 
     const pokemons = Array.from( Array(650) )
     
-    console.log( pokemons )
+    return pokemons.map( ( _ , index) => index + 1 )
+}
 
-    
+const getPokemonOptions = () => {
+
+    const mikedPokemons = getPokemons()
+        .sort(() => Math.random() - 0.5 )
+
+    getPokemonNames( mikedPokemons.splice(0, 4) )
 
 }
+
+const getPokemonNames = ( [a, b, c, d] = [] ) => {
+
+    console.log( a,b,c,d )
+
+}
+
+export default getPokemonOptions;
--- 03-pokemon-game/src/pages/PokemonPage.vue
+++ 03-pokemon-game/src/pages/PokemonPage.vue
@@ -9,8 +9,11 @@
 </template>
 
 <script>
-import PokemonPicture from '@/components/PokemonPicture';
-import PokemonOptions from '@/components/PokemonOptions';
+import PokemonPicture from '@/components/PokemonPicture'
+import PokemonOptions from '@/components/PokemonOptions'
+import getPokemonOptions from '@/helpers/getPokemonOptions'
+
+getPokemonOptions()
 
 export default {
     components: {
