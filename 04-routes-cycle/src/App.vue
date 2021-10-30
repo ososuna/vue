0959bd2fd5af380af17839f30697469d685ab7ca
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2021-10-30 15:34:40 -0500

    Added vue router

--- 04-routes-cycle/src/App.vue
+++ 04-routes-cycle/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <router-view />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AboutPage from './modules/pokemon/pages/AboutPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AboutPage
   }
 }
 </script>
