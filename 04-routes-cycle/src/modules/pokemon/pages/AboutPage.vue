f7e53c2b8e300d57d986fd397e0934a6e7eb602b
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2021-10-30 21:43:41 -0500

    Lifecycle methods

--- 04-routes-cycle/src/modules/pokemon/pages/AboutPage.vue
+++ 04-routes-cycle/src/modules/pokemon/pages/AboutPage.vue
@@ -1,3 +1,69 @@
 <template>
-    <h1>About page</h1>
+    <div>
+        <h1>About page</h1>
+        <hr>
+        <h2>{{ name }}</h2>
+        <button @click="onChangeName">
+            Change name
+        </button>
+    </div>
 </template>
+
+<script>
+export default {
+
+    data() {
+        return {
+            name: 'Oswald'
+        }
+    },
+
+    methods: {
+        onChangeName() {
+            this.name = 'Andy'
+        }
+    },
+
+    beforeCreate() {
+        console.log('beforeCreate')
+    },
+    created() {
+        // Peticiones HTTP
+        console.log('created')
+    },
+    beforeMount() {
+        console.log('beforeMount')
+    },
+    mounted() {
+        console.log('mounted')
+    },
+    beforeUpdate() {
+        console.log('beforeUpdate')
+    },
+    updated() {
+        console.log('updated')
+    },
+    activated() {
+        console.log('activated')
+    },
+    deactivated() {
+        console.log('deactivated')
+    },
+    beforeUnmount() {
+        console.log('beforeUnmount')
+    },
+    unmounted() {
+        // Limpieza
+        console.log('unmounted')
+    },
+    errorCaptured() {
+        console.log('errorCaptured')
+    },
+    renderTracked() {
+        console.log('renderTracked')
+    },
+    renderTriggered() {
+        console.log('renderTriggered')
+    }
+}
+</script>
