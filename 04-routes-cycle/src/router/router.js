1f48ca51ed7257bbaa493a24255cf92001771cde
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2021-11-01 22:26:06 -0600

    Layouts

--- 04-routes-cycle/src/router/router.js
+++ 04-routes-cycle/src/router/router.js
@@ -4,28 +4,61 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 const routes = [
   {
     path: '/',
-    redirect: '/home'
+    redirect: '/pokemon'
   },
   {
-    path: '/home',
-    name: 'home',
-    component: () => import('../modules/pokemon/pages/ListPage')
+    path: '/pokemon',
+    name: 'pokemon',
+    component: () => import('@/modules/pokemon/layouts/PokemonLayout'),
+    children: [
+      {
+        path: 'home',
+        name: 'pokemon-home',
+        component: () => import('../modules/pokemon/pages/ListPage')
+      },
+      {
+        path: 'about',
+        name: 'pokemon-about',
+        component: () => import('../modules/pokemon/pages/AboutPage')
+      },
+      {
+        path: 'pokemonid/:id',
+        name: 'pokemon-id',
+        component: () => import('../modules/pokemon/pages/PokemonPage'),
+        props: ({ params }) => {
+          const id = Number( params.id )
+          return isNaN( id ) ? { id: 1 } : { id }
+        }
+      },
+      {
+        path: '',
+        redirect: { name: 'pokemon-about' }
+      }
+    ]
   },
+
   {
-    path: '/about',
-    name: 'about',
-    /* Lazy load */
-    component: () => import('../modules/pokemon/pages/AboutPage')
+    path: '/dbz',
+    name: 'dbz',
+    component: () => import('@/modules/dbz/layouts/DragonBallLayout'),
+    children: [
+      {
+        path: 'characters',
+        name: 'dbz-characters',
+        component: () => import('@/modules/dbz/pages/Characters')
+      },
+      {
+        path: 'about',
+        name: 'dbz-about',
+        component: () => import('@/modules/dbz/pages/About')
+      },
+       {
+        path: '',
+        redirect: { name: 'dbz-characters' }
+      }
+    ]
   },
-  {
-    path: '/pokemonid/:id',
-    name: 'pokemon-id',
-    component: () => import('../modules/pokemon/pages/PokemonPage'),
-    props: ({ params }) => {
-      const id = Number( params.id )
-      return isNaN( id ) ? { id: 1 } : { id }
-    }
-  },
+
   {
     path: '/:pathMatch(.*)*',
     component: () => import('../modules/shared/pages/NoPageFound')
